bcc2555214ac09fd173da639a1022ffb2123b568
Eric Tobin <eatobin@gmail.com>
2021-02-07 18:16:22 -0600

    progress!!

--- Program.fs
+++ Program.fs
@@ -8,6 +8,21 @@ let from whom = sprintf "from %s" whom
 
 [<EntryPoint>]
 let main _ =
-    let message = from "F#!!!!!? Ericky to Karen!! Bye! New house!!" // Call the function
+    let message =
+        from "F#!!!!!? Ericky to Karen!! Bye! New house!!" // Call the function
+
     printfn "Hello world %s" message
+
+    let numbers = [ 0 .. 100 ]
+
+    let MyFun (l: int list): int =
+        l
+        |> List.filter (fun x -> x % 2 = 0)
+        |> List.map (fun x -> x * 2)
+        |> List.sum
+
+    let res = MyFun numbers
+
+    printfn "answer: %i" res
+
     0 // return an integer exit code
