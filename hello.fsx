c07967176c3eb3eed0800eea2ba65d3f73109049
Eric Tobin <eatobin@gmail.com>
2021-02-12 23:00:30 -0600

    getting fun

--- hello.fsx
+++ hello.fsx
@@ -1,1 +1,17 @@
-printfn "Hello World from F# and Erick Jones!!"
+printfn "Hello World from F# and Ericky Jones!! And one more."
+
+let rec sum aList =
+    match aList with
+    | [] -> 0
+    | x :: xs -> x + sum xs
+
+let s = sum [ 1 .. 10 ]
+printfn "answer = %i" s
+
+let rec boom i =
+    match i with
+    | 0 -> 999
+    | _ -> boom (i - 1)
+
+let res = boom 3
+printfn "boom = %i" res
