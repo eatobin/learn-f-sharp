f72c53d298e3e751176a8c851e809f26de140ea2
Eric Tobin <eatobin@gmail.com>
2021-02-14 13:20:16 -0600

    recur!!

--- Program.fs
+++ Program.fs
@@ -15,14 +15,44 @@ let main _ =
 
     let numbers = [ 0 .. 100 ]
 
-    let MyFun (l: int list): int =
+    let myFun (l: int list): int =
         l
         |> List.filter (fun x -> x % 2 = 0)
         |> List.map (fun x -> x * 2)
         |> List.sum
 
-    let res = MyFun numbers
+    let res = myFun numbers
 
     printfn "answer: %i" res
+
+    let rec len l =
+        match l with
+        | [] -> 0
+        | _ :: tail -> len tail + 1
+
+    let list1 = [ "a"; "b" ]
+    let res2 = len list1
+    let list2 = [ 1 .. 10 ]
+    let res3 = len list2
+    printfn "length l1: %i" res2
+    printfn "length l2: %i" res3
+
+    let lenR l =
+        let rec loop cur cnt =
+            match cur with
+            | [] -> cnt
+            | (_ :: tail) -> loop tail (cnt + 1)
+
+        loop l 0
+
+    let list10 = [ "a"; "b" ]
+    let res20 = lenR list10
+    let list20 = [ 1 .. 10 ]
+    let res30 = lenR list20
+    let list30 = [ 1 .. 100001 ]
+    let res40 = lenR list30
+    printfn "length l10: %i" res20
+    printfn "length l20: %i" res30
+    printfn "length l30: %i" res40
 
     0 // return an integer exit code
